f0e2a0158f8bdf6b9c9842033a6fb05b8aa1f331
Pax <varzinov@yandex.ru>
2018-01-11 00:54:39 +0300

    Адаптация графика для парка ТЗ

--- webapp/controller/TechState.controller.js
+++ webapp/controller/TechState.controller.js
@@ -7,37 +7,30 @@ sap.ui.define([
         onInit: function(){
             console.log('TECHSTATE_INIT');
 
-
             var oDataset = new openui5.simplecharts.SimpleChartData();
-            oDataset.bindDimensions({ items : [ { name: "label", description:"Equipa", axis: "x"},
-                {name:"value", description:"Ano", axis:"y"}]});
-            oDataset.bindMeasures({ items : [ { name: "value2", description:"Golos", rank: "1"}]});
-            var data = { "items": [
-                { "label": "Benfica", "value": "2014", "value2":"70"},
-                { "label": "Benfica", "value": "2013", "value2":"60"},
-                { "label": "Sporting", "value": "2014", "value2":"50"},
-                { "label": "Sporting", "value": "2013", "value2":"40"},
-                { "label": "FC Porto", "value": "2014", "value2":"30"},
-                { "label": "FC Porto", "value": "2013", "value2":"20"},
-                { "label": "Benfica", "value": "2012", "value2":"70"},
-                { "label": "Benfica", "value": "2011", "value2":"14"},
-                { "label": "Sporting", "value": "2012", "value2":"50"},
-                { "label": "Sporting", "value": "2011", "value2":"40"},
-                { "label": "FC Porto", "value": "2012", "value2":"30"},
-                { "label": "FC Porto", "value": "2011", "value2":"30"}
+            oDataset.bindDimensions({ items: [
+                {name: 'label', /*description: 'Наполненность парка',*/ axis: 'x'},
+                {name: 'value', /*description: '',*/ axis: 'y'}
+            ]});
+            oDataset.bindMeasures({ items: [
+                {name: 'value2', description: '%', rank: '1'}
+            ]});
+            var data = { items: [
+                { label: 'Цистерны', value: 'fuel', value2: 99.28},
+                { label: 'Цистерны', value: 'empty', value2: 0.72},
+                { label: 'Баки', value: 'fuel', value2: 64.98},
+                { label: 'Баки', value: 'empty', value2: 35.02}
             ]};
-            // var data = { "items": [
-            //     { "label": "Наполненность парка (цистерны ТЗ)", "value": "2014", "value2":"70"},
-            //     { "label": "Наполненность парка (цистерны ТЗ)", "value": "2013", "value2":"60"},
-            //     { "label": "Наполненность баков парка ТЗ", "value": "2014", "value2":"50"},
-            //     { "label": "Наполненность баков парка ТЗ", "value": "2013", "value2":"40"}
-            // ]};
             oDataset.bindData(data);
 
-            var chart = new openui5.simplecharts.SimpleStackedBarChart({ title: "Goals by Year"});
+            var chart = new openui5.simplecharts.SimpleStackedBarChart({
+                title: 'Наполненность парка ТЗ',
+                drawLegend: false,
+                colors: {fuel: '#fac364', empty: '#eff4f9'}
+            });
             chart.setDataSet(oDataset);
 
-            var panel = this.getView().byId("fuelchart");
+            var panel = this.getView().byId('fuelchart');
             panel.addContent(chart);
         }
     });
